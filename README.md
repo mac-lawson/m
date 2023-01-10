5f8a7538ab0c400029497c49a3f0231d1b2b2aad
mac lawson <macattackairman@gmail.com>
2023-01-09 21:25:30 -0500

    commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+![Logo](m%2B%2B.png)
 # M++
 ----------------------
-## A language you can understand
-## Documentation
+### A language you can understand
+### Documentation
 ---
+
+#### Basic Operations
--- interpreter/lex.py
+++ interpreter/lex.py
@@ -1,14 +1,34 @@
 # Official Language Interpreter for M++
 # Official Language Interpreter for M++
 
-
 import sys
+import matplotlib.pyplot as plt
+def err(skk): print("\033[91m {}\033[00m" .format(skk))
+def fix(skk): print("\033[92m {}\033[00m" .format(skk))
+
+# Graphing Library for M++
+
+def plot_numbers(x, y):
+  plt.scatter(x, y)
+  plt.xlabel('x')
+  plt.ylabel('y')
+  plt.title('M++ Graphing')
+  plt.show()
 
 # Math Library for M++
 def math(line):
-    if(line[0] == "Math.Average"):
-        print((float(line[1]) + float(line[3])) / 2)
-
+    try:
+        if(line[0] == "Math.Average"):
+            print((float(line[1]) + float(line[3])) / 2)
+        if(line[0] == "Math.Max"):
+            if(float(line[1]) > float(line[3])):
+                print(float(line[1]))
+            else:
+                print(float(line[3]))
+        if(line[0] == "Math.Graph"):
+            plot_numbers(float(line[1]), float(line[3]))
+    except:
+        err("m++ interpreter error")
 # Official Lexer for M++
 # All language libraries should be applied
 def lex(line):  
@@ -23,20 +43,30 @@ def lex(line):
     if(line[0] == "breakln()"):
         print("")
 
-def process_file(filename):
+def lex_file(filename):
     line_num = 1
     with open(filename) as f:
         for line in f:
             words = line.split()
-            globals()[f'line_{line_num}'] = words
-            line_num += 1
-            # Keep print(words) for debugging
-            # print(words)
-            if("Math" in words):
-                math(words)
-            else:    
-                lex(words)
+            # Comments and blank spaces
+            if("//" in words):
+                i = 0
+            if(len(words) == 0):
+                i = 0
+            else:
+                globals()[f'line_{line_num}'] = words
+                line_num += 1
+                # Keep print(words) for debugging
+                # print(words)
+                if("Math" in words[0]):
+                    math(words)
+                else:    
+                    lex(words)
 
-# Example usage
-process_file('demo.m++')
-
+# try:
+lex_file(sys.argv[1])
+# except:
+#     err("m++ error")
+#     print("No file was provided to the m++ compiler")
+#     fix("\nHow to fix: ")
+#     print("./m++ my_file.m++")
